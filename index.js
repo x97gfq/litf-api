4fe60c82a9c36a610af770c7cf7adc1d49e00d23
Jamie Symonds <x97gfq@hotmail.com>
2020-11-07 21:14:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/characters', (req, res) => {
+  console.log('call to api/characters received');
+  var response = [{"id":1,"name":"bird"},{"id":1,"name":"wolf"},{"id":1,"name":"bear"}];
+  res.json(response);
+})
+
+
+app.listen(8081);
+console.log('Listening on http://localhost:8081');
