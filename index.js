ee626b9febfdde34e6d99fccee0f7f060eb9cb56
Jamie Symonds <x97gfq@hotmail.com>
2020-12-06 19:17:15 -0400

    animal properties

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.get('/api/animals', (req, res) => {
   console.log('call to api/animals received');
   //this list should be stored in a database
   var response = [
-    {"id":1,"name":"raven"},
-    {"id":2,"name":"wolf"},
-    {"id":3,"name":"deer"},
-    {"id":4,"name":"rabbit"},
-    {"id":5,"name":"owl"},
-    {"id":6,"name":"fox"},
-    {"id":7,"name":"bear"},
-    {"id":8,"name":"lynx"}
+    {"id":1,"name":"raven","eyes":"blinking_eyes_test.gif","image":"raven.jpg","sound":"raven.wav"},
+    {"id":2,"name":"wolf","eyes":"blinking_eyes_test.gif","image":"wolf.jpg","sound":"wolf.wav"},
+    {"id":3,"name":"deer","eyes":"blinking_eyes_test.gif","image":"deer.jpg","sound":"deer.wav"},
+    {"id":4,"name":"rabbit","eyes":"blinking_eyes_test.gif","image":"rabbit.jpg","sound":"rabbit.wav"},
+    {"id":5,"name":"owl","eyes":"blinking_eyes_test.gif","image":"owl.jpg","sound":"owl.wav"},
+    {"id":6,"name":"fox","eyes":"blinking_eyes_test.gif","image":"fox.jpg","sound":"fox.wav"},
+    {"id":7,"name":"bear","eyes":"blinking_eyes_test.gif","image":"bear.jpg","sound":"bear.wav"},
+    {"id":8,"name":"lynx","eyes":"blinking_eyes_test.gif","image":"lync","sound":"lynx.wav"}
   ];
   res.json(response);
 })
